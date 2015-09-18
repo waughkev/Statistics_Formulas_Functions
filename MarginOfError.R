2561a1d1f728badd0b37cfbfd6a804882b85c972
Kevin Waugh <waughkev@gmail.com>
2015-09-18 12:35:42 -0400

    Added MarginOfError.R

---
+++ MarginOfError.R
@@ -0,0 +1,17 @@
+##Margin of error
+##determine what the confidence level is in a equation 
+
+##Console Script, run the following lines inside the R Console to get the answer
+sample <- 100 #Size of sample
+standarddeviation <- 10 #Standard deviation of sample
+marginOfError <- (2 * standarddeviation)/sqrt(sample)
+marginOfError
+
+##function View
+MarginOfError <- function(sample,standarddeviation) {
+  MOfE <- <- (2 * standarddeviation)/sqrt(sample)
+  return(MOfE)
+}
+
+##Sample line to run function after function created inside R.
+MarginofError(100,10)
